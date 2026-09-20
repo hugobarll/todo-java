d15e5bca3057acb259056961966f1ad612576d58
Victor Hugo <victorhugo.prog@gmail.com>
2026-09-20 07:36:17 -0300

    feat: create TaskManager class and implement task creation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,36 @@
 package app;
 
-public class Main {
+import services.TaskManager;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        TaskManager taskManager = new TaskManager();
+
+        while (true) {
+            System.out.println("1 - Add task");
+            System.out.println("2 - List tasks");
+            System.out.println("3 - Change status");
+            System.out.println("4 - Delete task");
+            System.out.println("5 - Exit");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            if (option == 1) {
+                System.out.print("Enter the task: ");
+                String description = sc.nextLine();
+                taskManager.addTask(description);
+            }
+
+            if (option == 5) {
+                break;
+            }
+        }
+
+        sc.close();
+    }
 }
---
+++ src/main/java/services/TaskManager.java
@@ -0,0 +1,19 @@
+package services;
+
+import entities.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+
+    private List<Task> tasks = new ArrayList<>();
+
+    int nextId = 1;
+
+    public void addTask(String description) {
+        Task task = new Task(nextId, description, false);
+        nextId++;
+        tasks.add(task);
+    }
+}
