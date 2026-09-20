34bd394883858817865a6eda303c3ff98d99263c
Victor Hugo <victorhugo.prog@gmail.com>
2026-09-20 05:34:27 -0300

    feat: add Task entity

---
+++ src/main/java/entities/Task.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private boolean done;
+
+    public Task(int id, String description, boolean done) {
+        this.id = id;
+        this.description = description;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", done=" + done +
+                '}';
+    }
+}
